658cb4362f126d1b96fd5e4b8bb947684d72c11a
Joe Loser <joeloser93@gmail.com>
2020-03-15 17:19:18 -0400

    exercise1 complete

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
-    ) where
+( someFunc, toDigits, toDigitsRev
+) where
+
+-- Exercise 1
+toDigits :: Integer -> [Integer]
+toDigits x
+    | x <= 0 = []
+    | x < 10 = [x]
+    | otherwise = toDigits (x `div` 10) ++ [x `mod` 10]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x = reverse (toDigits x)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+main = hspec $ do
+    describe "Exercise 1" $ do
+        it "converts 0 to empty list" $ do
+            toDigits 0 `shouldBe` ([] :: [Integer])
+        it "converts negative number to empty list" $ do
+            toDigits (-1) `shouldBe` ([] :: [Integer])
+        it "converts an integer into list of digits" $ do
+            toDigits 1234 `shouldBe` ([1, 2, 3, 4] :: [Integer])
+        it "toDigitsRev converts an integer into list of digits reversed" $ do
+            toDigitsRev 1234 `shouldBe` ([4, 3, 2, 1] :: [Integer])
+        it "toDigitsRev converts an integer into list of digits reversed on empty list" $ do
+            toDigitsRev (-17) `shouldBe` ([] :: [Integer])
