97dc51cf2bc3c80d716fad2a5c9f8934eab0ab74
Joe Loser <joeloser93@gmail.com>
2020-04-20 22:28:01 -0400

    Homework 5 exercise 4

--- src/Homework5.hs
+++ src/Homework5.hs
@@ -51,3 +51,35 @@ reifyTest = and
     (reify $ add (lit 2) (lit 3)) == Add (Lit 2) (Lit 3),
     (reify $ mul (lit 2) (lit 3)) == Mul (Lit 2) (Lit 3)
   ]
+
+
+-- Exercise 4
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (<= 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit x =  Mod7 $ x `mod` 7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMinMax = testExp :: Maybe MinMax
+testMod7 = testExp :: Maybe Mod7
