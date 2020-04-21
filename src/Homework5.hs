ab641e1eeafba1ebad19c1f275e9cd1db13d0f9c
Joe Loser <joeloser93@gmail.com>
2020-04-20 22:10:32 -0400

    Homework 5 exercise 3

--- src/Homework5.hs
+++ src/Homework5.hs
@@ -9,7 +9,6 @@ eval (Lit x) = x
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
 
-
 evalTest = and
   [
   eval (Lit 2) == 2,
@@ -28,4 +27,27 @@ evalStrTest = and
   evalStr "3 + 5" == Just 8,
   evalStr "3 * 2" == Just 6,
   evalStr "3 + * 2" == Nothing
+  ]
+
+
+-- Exercise 3
+class Expr x where
+  lit :: Integer -> x
+  add :: x -> x -> x
+  mul :: x -> x -> x
+
+instance Expr ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
+
+-- Given function to constrain the type of its arg to ExprT
+reify :: ExprT -> ExprT
+reify = id
+
+reifyTest = and
+  [
+    (reify $ lit 2) == Lit 2,
+    (reify $ add (lit 2) (lit 3)) == Add (Lit 2) (Lit 3),
+    (reify $ mul (lit 2) (lit 3)) == Mul (Lit 2) (Lit 3)
   ]
