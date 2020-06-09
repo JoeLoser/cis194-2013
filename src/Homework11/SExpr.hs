c909220e2a475ab818f5a783d8afedb6b4f5d068
Joe Loser <joeloser93@gmail.com>
2020-06-08 22:49:03 -0400

    Homework 11 exercise 1

---
+++ src/Homework11/SExpr.hs
@@ -0,0 +1,62 @@
+{- CIS 194 HW 11
+   due Monday, 8 April
+-}
+
+module SExpr where
+
+import AParser
+import Control.Applicative
+
+import Data.Char
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = liftA2 (:) p (zeroOrMore p)
+{-Same as -}
+{-oneOrMore p = (:) <$> p <*> zeroOrMore p-}
+
+testZeroOrMore = and
+  [
+    runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH" == Just ("ABC", "dEfgH"),
+    runParser (zeroOrMore (satisfy isUpper)) "abcdeFGh" == Just ("", "abcdeFGh")
+  ]
+
+testOneOrMore = and
+  [
+    runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH" == Just ("ABC", "dEfgH"),
+    runParser (oneOrMore (satisfy isUpper)) "abcdeFGh" == Nothing
+  ]
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = undefined
+
+ident :: Parser String
+ident = undefined
+
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
+type Ident = String
+
+-- An "atom" is either an integer value or an identifier.
+data Atom = N Integer | I Ident
+  deriving Show
+
+-- An S-expression is either an atom, or a list of S-expressions.
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
